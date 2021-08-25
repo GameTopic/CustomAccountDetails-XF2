0f6fe544b84678d1fea6932a9b786ce4df528feb
NICK97 <40327737+bolandnick18@users.noreply.github.com>
2021-08-24 19:34:14 -0700

    Updated to XenForo 2.2

---
+++ Setup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace nick97\CustomAccountDetail;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}
